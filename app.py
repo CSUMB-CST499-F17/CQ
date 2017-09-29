b3c865e7cdaf8dd1145b682a42d94b7878394c81
Katelynn Jones <katjones419@gmail.com>
2017-09-29 20:58:24 +0000

    some postgresql stuff

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-import os, flask, flask_socketio, time
+import os, flask, flask_socketio, flask_sqlalchemy, time
 
 app = flask.Flask(__name__)
 socketio = flask_socketio.SocketIO(app)
+
+import models
 
 @app.route('/')
 def hello():
     return flask.render_template('index.html')
     
 if __name__ == '__main__':
-   
     socketio.run(
             app,
             host=os.getenv('IP', '0.0.0.0'),
