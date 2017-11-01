68b0868437108e82460b3901c976a28f9a57f8b4
Katelynn Jones <katjones419@gmail.com>
2017-11-01 05:21:09 +0000

    frontpage css update, slideshow

--- scripts/home.js
+++ scripts/home.js
@@ -7,6 +7,10 @@ export class Home extends React.Component {
     constructor(props) {
         super(props);
         this.pageName = 'home';
+        this.index = 0;
+        this.images = ['boats','bust','canneryrow','crossedarms','lighthousewide','montereycanningcompany','sistercitypark','swanboat','whale'];
+        // IMAGES THAT SHOW UP SIDEWAYS: 'diversmemorial','lady','lighthousenarrow','shareabench','twowhales', 'yesterdaysdream'
+        this.showSlides = this.showSlides.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
     componentDidMount(){
@@ -16,9 +20,11 @@ export class Home extends React.Component {
                 if(savedPage == 'null'){
                     savedPage = 'home';
                 }
-                console.log(savedPage);
                 if (savedPage != 'home'){
                     this.props.changePage(this.pageName,savedPage);
+                }
+                else{
+                    this.showSlides();
                 }
             }catch(e){}
         });
@@ -26,22 +32,40 @@ export class Home extends React.Component {
     handleSubmit(event) {
         event.preventDefault();
     }
+    showSlides() {
+        var image = document.getElementById("ss-image");
+        if (this.index < this.images.length){
+            image.src="../static/image/gallery/"+this.images[this.index]+".jpg";
+            this.index+=1;
+        }
+        else{
+            this.index=0;
+        }
+        // var index = Math.floor(Math.random() * this.images.length);
+        // image.src="../static/image/gallery/"+this.images[index]+".jpg"; 
+        if(this.props.lastPage == 'home'){
+            setTimeout(this.showSlides, 10000); // Change image every 10 seconds
+        }
+    }
     render() {
 
         return (
             <div>
-                <div id='header'>
+                <div id='front-header'>
                     <img id="logo-big" src="../static/image/logo-big.png"/>
                 </div>
                 <div id='intro'>
-                    <img id = "pageImage" src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Lovers_Point_Park_-_Pacific_Grove%2C_CA_-_DSC06525.JPG/1200px-Lovers_Point_Park_-_Pacific_Grove%2C_CA_-_DSC06525.JPG' width='40%'></img>
-                </div>
-                <div id='buttons'>
-                    <div className ="tool">
-                        <button className="btn" onClick={() => this.props.changePage('explore')}>Let's Explore!</button>
-                        <button className="btn" onClick={() => this.props.changePage('existingTeam')}>Log into Existing Team</button>
-                        <button className="btn" onClick={() => this.props.changePage('adminHome')}>Temp Button to Admin Homepage</button>
-                        <button className="btn" onClick={() => this.props.changePage('play')}>Temp Button to Play Page</button>
+                   <div id="slideshow">
+                        <div className='helper'></div><img id="ss-image" src="../static/image/gallery/boats.jpg"/>
+                    </div>
+                
+                    <div id='buttons'>
+                        <div className ="tool">
+                            <button className="btn" onClick={() => this.props.changePage('explore')}>Let's Explore!</button>
+                            <button className="btn" onClick={() => this.props.changePage('existingTeam')}>Log into Existing Team</button>
+                            <button className="btn" onClick={() => this.props.changePage('adminHome')}>Temp Button to Admin Homepage</button>
+                            <button className="btn" onClick={() => this.props.changePage('play')}>Temp Button to Play Page</button>
+                        </div>
                     </div>
                 </div>
             </div>
